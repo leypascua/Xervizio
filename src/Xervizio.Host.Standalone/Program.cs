116da65d25f7cbf9379ccd893bb11cc8d41b98e3
Harley Pascua <leypascua@yahoo.com>
2013-11-20 16:57:56 +0800

    First commit
    Git noob here.. this may get messy

---
+++ src/Xervizio.Host.Standalone/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Topshelf;
+using Topshelf.HostConfigurators;
+using Topshelf.ServiceConfigurators;
+
+namespace Xervizio.Host.Standalone {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
